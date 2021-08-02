82d07814efe19893a41121dab042fe39c77a7bcf
eziocode <79853490+eziocode@users.noreply.github.com>
2021-08-02 18:42:05 +0530

    Add files via upload

--- CodeYT.py
+++ CodeYT.py
@@ -30,12 +30,13 @@ c.create_image(0,0,image=img,anchor='nw')
 
 #for text
 
-c.create_text(700,100,text='YOUTUBE DOWNLOADER',fill='red',font=('Tahoma Bold',24),anchor=N)
-c.create_text(700,190,text='Paste Link Here ⬇',fill='red',font=('Tahoma Bold',24),anchor=N)
+c.create_text(700,100,text='YOUTUBE DOWNLOADER',fill='#ec8a6c',font=('Tahoma Bold',24),anchor=N)
+c.create_text(700,190,text='Paste Link Here ⬇',fill='#ec8a6c',font=('Tahoma Bold',24),anchor=N)
 
 #for entry box
 
 link = StringVar()
+
 
 link_enter = Entry(root, width = 50,textvariable = link,fg = 'blue',bg = 'light grey',justify=CENTER).place(relx=0.5, rely=0.4, anchor=CENTER)
 
@@ -50,7 +51,7 @@ def hddownload():
     
     else:
         video = url.streams.get_highest_resolution()
-        video = video.download()
+        video = video.download('C:/YTvideo')
         mb.showinfo('Yes!',"Download Completed✔")
 
 
@@ -62,8 +63,8 @@ def sddownload():
         mb.showerror('OOPS',"Please,Enter a valid input❌")
     
     else:
-        video = url.streams.first()
-        video = video.download()
+        video = url.streams.filter(progressive=True,file_extension='mp4').first()
+        video = video.download(output_path = 'C:/YTvideo')
         mb.showinfo('Yes!',"Download Completed✔")
 
 def readme1():
@@ -72,6 +73,9 @@ def readme1():
 def web():
     return wb.open('https://github.com/eziocode/eziocode')
 
+def down():
+    return wb.open('C:\YTvideo')
+
 #for clock widget 
 
 clck = Label(c,font=("Courier" ,20),bg='#c8bfb6',fg='black',bd=1)
@@ -79,17 +83,18 @@ clck.place(x=15,y=15)
 
 def timer():
     input = time.strftime("%H:%M:%S")
-    clck.config(text=input)
+    clck.config(text=input,fg='#ed6f9d')
     clck.after(200,timer)
 
 timer()
 
 #for buttons
 
-butn = PhotoImage(file='wd.png')
+butn = PhotoImage(file='hd.png')
 butn1 = PhotoImage(file='sd.png')
 readme = PhotoImage(file='readme.png')
 giti =  PhotoImage(file='git.png')
+downl = PhotoImage(file='folder.png')
 
 button1 = Button(root,image=butn,borderwidth=0,bg="#dfada4",command=hddownload)
 button2 = Button(root,image=butn1,bd=0,bg="#daa39c",command=sddownload)
@@ -97,28 +102,14 @@ button2 = Button(root,image=butn1,bd=0,bg="#daa39c",command=sddownload)
 button = c.create_window(850,250,anchor='nw',window=button1)
 button_ = c.create_window(850,290,anchor='nw',window=button2)
 
-read_text = c.create_text(1250,590,text='Readme...',font=('arial bold',12),fill='grey')
+read_text = c.create_text(1250,550,text='Readme...',font=('arial bold',12),fill='grey')
 
-info = c.create_window(1250,605, anchor='nw',window=Button(root,image=readme,borderwidth=0,bg='#adb0b5',command=readme1))
+info = c.create_window(1250,565, anchor='nw',window=Button(root,image=readme,borderwidth=0,bg='#adb0b5',command=readme1))
 
 git = c.create_window(1250,650,anchor='nw',window=Button(root,image=giti,borderwidth=0,bg='#adb0b5',command=web))
+
+downn_file = c.create_window(1250,610,anchor='nw',window=Button(root,image=downl,borderwidth=0,bg='#adb0b5',command=down))
 
 #to start
 
 root.mainloop()
-
-
-
-
-
-'''''
- ▄████▄   ▒█████  ▓█████▄ ▓█████     ▄▄▄▄ ▓██   ██▓   ▓█████ ▒███████▒ ██▓ ▒█████  
-▒██▀ ▀█  ▒██▒  ██▒▒██▀ ██▌▓█   ▀    ▓█████▄▒██  ██▒   ▓█   ▀ ▒ ▒ ▒ ▄▀░▓██▒▒██▒  ██▒
-▒▓█    ▄ ▒██░  ██▒░██   █▌▒███      ▒██▒ ▄██▒██ ██░   ▒███   ░ ▒ ▄▀▒░ ▒██▒▒██░  ██▒
-▒▓▓▄ ▄██▒▒██   ██░░▓█▄   ▌▒▓█  ▄    ▒██░█▀  ░ ▐██▓░   ▒▓█  ▄   ▄▀▒   ░░██░▒██   ██░
-▒ ▓███▀ ░░ ████▓▒░░▒████▓ ░▒████▒   ░▓█  ▀█▓░ ██▒▓░   ░▒████▒▒███████▒░██░░ ████▓▒░
-░ ░▒ ▒  ░░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░   ░▒▓███▀▒ ██▒▒▒    ░░ ▒░ ░░▒▒ ▓░▒░▒░▓  ░ ▒░▒░▒░ 
-  ░  ▒     ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░   ▒░▒   ░▓██ ░▒░     ░ ░  ░░░▒ ▒ ░ ▒ ▒ ░  ░ ▒ ▒░ 
-░        ░ ░ ░ ▒   ░ ░  ░    ░       ░    ░▒ ▒ ░░        ░   ░ ░ ░ ░ ░ ▒ ░░ ░ ░ ▒  
-░ ░          ░ ░     ░       ░  ░    ░     ░ ░           ░  ░  ░ ░     ░      ░ ░  
-░                  ░                      ░░ ░               ░                     '''
